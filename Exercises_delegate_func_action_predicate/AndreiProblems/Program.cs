29dd5377fd2727e4d24a8defae374675859e83b0
viorelaBudianu <viorela.hariga@yahoo.com>
2019-06-05 16:45:14 +0300

    Main from Andrei's link

---
+++ Exercises_delegate_func_action_predicate/AndreiProblems/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreiProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allUsers = ReadUsers("users.json");
+            var allPosts = ReadPosts("posts.json");
+
+            // 1 - find all users having email ending with ".net".
+            var users1 = from u in allUsers
+                         where u.Email.EndsWith(".net")
+                         select u;
+
+            var users2 = allUsers.Where(x => x.Email.EndsWith(".net"));
+
+            var emails = allUsers.Select(x => x.Email).ToList();
+
+            // 2 - find all posts for users having email ending with ".net".
+
+
+            // 3 - print number of posts for each user.
+
+
+            // 4 - find all users that have lat and long negative.
+
+
+            // 5 - find the post with longest body.
+
+
+            // 6 - print the name of the employee that have post with longest body.
+
+
+            // 7 - select all addresses in a new List<Address>. print the list.
+
+
+            // 8 - print the employee with min lat
+
+
+            // 9 - print the employee with max long
+
+
+            // 10 - create a new class: public class UserPosts { public User User {get; set}; public List<Post> Posts {get; set} }
+            //    - create a new list: List<UserPosts>
+            //    - insert in this list each user with his posts only
+
+
+            // 11 - order users by zip code
+
+
+            // 12 - order users by number of posts
+        }
+
+        private static List<Post> ReadPosts(string file)
+        {
+            return ReadData.ReadFrom<Post>(file);
+        }
+
+        private static List<User> ReadUsers(string file)
+        {
+            return ReadData.ReadFrom<User>(file);
+        }
+    }
+}
+    }
+}
